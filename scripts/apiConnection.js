db05ae07bc8313de0f0d1f8813c46c17f6fffbc8
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-10-07 11:14:29 +0200

    first try

---
+++ scripts/apiConnection.js
@@ -0,0 +1,28 @@
+let body = document.querySelector("body")
+
+/*let beerApiConnection = async () => {
+    const response = await fetch("https://api.punkapi.com/v2/beers")
+    const jason = await response.json()
+
+    for (i=0; i < 25; i++){
+    let article = document.createElement("img")
+    article.setAttribute("src", jason[i].image_url)
+    body.appendChild(article)
+    console.log(jason[i])
+    }
+}
+
+beerApiConnection();*/
+
+const key = "3848e1696e988e3b239b560c608dbd8b"
+const proxy = "https://cors-anywhere.herokuapp.com/"
+const apiUrl = "https://sandbox-api.brewerydb.com/v2/"
+
+let beerApiConnection2 = async () => {
+
+    const response = await fetch(`${proxy + apiUrl}beer/random/?key=${key}`)
+    const jason = await response.json()
+    console.log(jason)
+    }
+
+beerApiConnection2()
